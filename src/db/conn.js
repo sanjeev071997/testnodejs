a515b105dc074909d8927732e986b550dfe2657e
sanjeev <sachinsaini19970@gmail.com>
2021-09-01 22:09:55 +0530

    adding scripts

--- src/db/conn.js
+++ src/db/conn.js
@@ -1,9 +1,9 @@
 const mongoose = require("mongoose");
-mongoose.connect(process.env.MONGODB_URL || "mongodb://localhost:27017/blogFrom", {
+mongoose.connect( process.env.MONGODB_URL || "mongodb://localhost:27017/blogFrom", {
     useNewUrlParser:true,
     useUnifiedTopology: true
 }).then (()=>{
     console.log("connection successful ");
 }).catch ((error)=>{
     console.log(error);
-})
+});
